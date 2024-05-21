0664177636dff15da6fda880e210144d4958d274
Tanishq Sethi <tanishqsethi05@gmail.com>
2024-05-21 18:17:13 +0530

    java script dom 3

--- javascript-dom/dom.js
+++ javascript-dom/dom.js
@@ -94,17 +94,58 @@
 // newDiv.style.fontSize='30px';
 
 //event listener
-var button=document.getElementById('button').addEventListener
-('click',buttonClick);
-function buttonClick(e){
-    // console.log('button click');
-    // document.getElementById('header-title').textContent='Changed';
-    // document.querySelector('#main').style.backgroundColor='#f4f4f4';
-    // console.log(e);
+// var button=document.getElementById('button').addEventListener
+// ('click',buttonClick);
+// function buttonClick(e){
+//     // console.log('button click');
+//     // document.getElementById('header-title').textContent='Changed';
+//     // document.querySelector('#main').style.backgroundColor='#f4f4f4';
+//     // console.log(e);
 
-    console.log(e.target);
-    console.log(e.target.className);
-    console.log(e.target.id);
-    var output=getElementById('output');
-    output.innerHTML=
+//     // console.log(e.target);
+//     // console.log(e.target.className);
+//     // console.log(e.target.id);
+//     // var output=document.getElementById('output');
+//     // output.innerHTML='<h3>'+e.target.id+'</h3>';
+//     // console.log(e.type);
+
+//     // console.log(e.clientX);//gives the position of the mouce in x axis
+//     // console.log(e.clientY);
+//     //these give positionn from browser window
+
+//     // console.log(e.offsetX);//from the actual element 
+//     //similary for y 
+
+//     // console.log(e.altKey);//tells if the alt key is pressed while clicking
+//     //similary for ctrl and shift
+// }
+
+// var button=document.getElementById('button')
+// var box=document.getElementById('box')
+// button.addEventListener('click',runEvent);
+// button.addEventListener('dblclick',runEvent);
+// button.addEventListener('mousedown',runEvent);
+// button.addEventListener('mouseup',runEvent);
+
+// box.addEventListener('mouseenter',runEvent);
+// box.addEventListener('mouseleave',runEvent);
+// box.addEventListener('mouseover',runEvent);
+// box.addEventListener('mouseout',runEvent);
+// box.addEventListener('mousemove',runEvent);
+
+var itemInput=document.querySelector('input[type="text"]');
+var form =document.querySelector('form');
+var select=document.querySelector('select');
+// itemInput.addEventListener('keydown',runEvent);
+
+form.addEventListener('submit',runEvent)
+
+// select.addEventListener('change',runEvent);
+function runEvent(e){
+    e.preventDefault();
+    console.log('EVENT TYPE: '+e.type);
+    // console.log(e.target.value);
+    // output.innerHTML='<h3>MouseX: '+e.offsetX+ '</h3><h3>MouseY: '+e.offsetY+'</h3>';
+
+    // document.body.style.backgroundColor='rgb('+e.offSetX+','+e.offSetY+',40)';
 }
