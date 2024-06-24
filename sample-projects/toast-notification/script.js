d8a17a327181a5427f9943bee2a5b3c367e29eb1
Tanishq Sethi <tanishqsethi05@gmail.com>
2024-06-24 16:42:07 +0530

    js dom project day 27

---
+++ sample-projects/toast-notification/script.js
@@ -0,0 +1,21 @@
+const btn=document.getElementById('button')
+const toasts=document.getElementById('toasts')
+const messages=[
+    'Message One',
+    'Message two',
+    'Message three',
+    'Message four',
+]
+btn.addEventListener('click',()=>createNotification())
+function createNotification(){
+    const notif=document.createElement('div')
+    notif.classList.add('toast')
+    notif.innerText=getRandomMessage()
+    toasts.appendChild(notif)
+    setTimeout(()=>{
+        notif.remove()
+    },3000)
+}
+function getRandomMessage(){
+    return messages[Math.floor(Math.random()*messages.length)]
+}
