a16fbf6237612850dcb891e0a76bfc25077995c5
Tanishq Sethi <tanishqsethi05@gmail.com>
2024-06-26 11:19:51 +0530

    js dom project day 32

---
+++ sample-projects/checkbox/script.js
@@ -0,0 +1,20 @@
+const  toggles=document.querySelectorAll('.toggle')
+const good=document.querySelector('#good')
+const cheap=document.querySelector('#cheap')
+const fast=document.querySelector('#fast')
+toggles.forEach(toggle=>toggle.addEventListener('change',(e)=>doStuff(e.target)))
+function doStuff(clickedOne){
+    if(good.checked&&cheap.checked&&fast.checked){
+        if(good===clickedOne){
+            fast.checked=false
+        }
+        if(cheap===clickedOne){
+            good.checked=false
+        }
+        if(fast===clickedOne){
+            cheap.checked=false
+        }
+
+    }
+}
+
